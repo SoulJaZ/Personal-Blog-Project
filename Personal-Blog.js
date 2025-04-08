0f1bb23064f34e87eeaadf57b08dfca903f2e69e
RasSoul <mmagtc1101@gmail.com>
2025-04-08 11:55:45 -0500

    Creación nueva carpeta ´articulos´ y archivo JSON, se creo función para obtener los articulos del archivo JSON mencionado anteriormente.

--- Personal-Blog.js
+++ Personal-Blog.js
@@ -4,6 +4,38 @@ const fs = require("fs"); // Leer y escribir los archivos.
 const path = require("path") // Gestionar rutas. 
 const querystring = require("querystring"); // Leer datos envíados desde los formularios.
 
+// Definir puerto en donde funcionará el servidor. 
+const PORT = 3000;
+
+// Contraseña para acceder al panel de administración.
+const ADMIN_PASSWORD = "1234";
+
+
+// Función para leer todos los articulos desde la carpeta 'articulos'.
+function obtenerArticulos(){
+
+    // representa la ruta de la carpeta que contendrá los articulos.
+    const archivos = fs.readFileSync('./articulos');
+
+    // representa el archivo json usado para almacenar la data de los articulos.
+    const archivoArticulos = fs.readFileSync("./articulos/archivoArticulos.json");
+
+    // representa el conjunto de los articulos con un elemento vacio, para almacenar todos los articulos. 
+    const articulos = [];
+
+    // Recorrer el archivo .json de la carpeta 'articulos'
+    archivos.forEach(archivoArticulos => {
+        const contenido = archivoArticulos;
+        const articulo = JSON.parse(contenido);
+        articulo.filename = archivoArticulos;
+        articulos.push(articulo);
+
+    });
+
+    return articulos;
+
+};
+
 
 // Crear Servidor.
 const servidor = http.createServer((req, res) => {
